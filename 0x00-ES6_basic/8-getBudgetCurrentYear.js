e1b2321dc5007428d0f6a765db9ac1ae193cfb7b
DevBasem <engr.basemahmed@gmail.com>
2024-05-27 18:10:34 -0700

    task 8 - 12

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
