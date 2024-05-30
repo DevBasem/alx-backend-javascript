d4a6d89cc12b35b5de1d7b2f288572b904ded6ed
DevBasem <engr.basemahmed@gmail.com>
2024-05-30 11:24:52 -0700

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
