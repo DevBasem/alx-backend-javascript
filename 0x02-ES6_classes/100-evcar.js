16a0c7b10004677b7b331c904356c8f0a8bd5292
DevBasem <engr.basemahmed@gmail.com>
2024-05-30 09:36:12 -0700

    task 1 adv

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
