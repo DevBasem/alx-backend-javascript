cfdb15340498a7f2e456d55d5fb1c6dc70105ef1
DevBasem <engr.basemahmed@gmail.com>
2024-05-30 11:11:07 -0700

    task 3 - end

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResponse, userResponse] = results;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
