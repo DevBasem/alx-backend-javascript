237098ffcfa24357932467917a4f6d872adc800b
DevBasem <engr.basemahmed@gmail.com>
2024-06-06 11:10:09 -0700

    task 0

---
+++ alx-backend-python/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two floats.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their sum.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
