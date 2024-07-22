cefa864860b94277196057cc502e97c8c9a8caec
DevBasem <engr.basemahmed@gmail.com>
2024-07-22 11:29:15 -0700

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+  res.end();
 });
 
-app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
