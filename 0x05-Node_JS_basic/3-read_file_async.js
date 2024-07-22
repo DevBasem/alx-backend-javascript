8176e9ac21d17c979b519b64e061cbc864d17dce
DevBasem <engr.basemahmed@gmail.com>
2024-07-22 11:23:42 -0700

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = lines.slice(1).map((line) => line.split(',')).filter((fields) => fields.length === 4);
+    const numberOfStudents = students.length;
+
+    console.log(`Number of students: ${numberOfStudents}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const field = student[3];
+      const firstName = student[0];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstName);
+    });
+
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
