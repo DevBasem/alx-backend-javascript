b1fb86d832bf014705e7ef2a975d9c931c0fa6a4
DevBasem <engr.basemahmed@gmail.com>
2024-07-22 10:57:29 -0700

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.once('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
